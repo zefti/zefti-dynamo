6bbb628bd61cd6b1f61e51899c61893f22100b4f
RYAN OGLE <ryanogle@RYANs-MacBook-Air-2.local>
2016-02-11 01:23:49 -0800

    stuff

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var AWS = require('aws-sdk');
 
 
-module.exports = function(config){
+module.exports = function(options){
+  var dataSource = options.dataSource;
   AWS.config.update({region: 'us-east-1'});
-  AWS.config.update({accessKeyId: config.accessKeyId, secretAccessKey: config.secretAccessKey});
+  AWS.config.update({accessKeyId: dataSource.accessKeyId, secretAccessKey: dataSource.secretAccessKey});
   var dynamodb = new AWS.DynamoDB();
   return dynamodb;
 };
